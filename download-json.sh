e98f56fe64b6eae1e4f361de3fa36fa260231eb2
Phil de Joux <philderbeast@gmail.com>
2018-12-26 13:24:23 -0500

    Update the json with scoring stage as part of path.

--- download-json.sh
+++ download-json.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 mkdir json
-curl -X GET -H "Accept:application/json" http://localhost:3000/comps | jq > json/comps.json
-curl -X GET -H "Accept:application/json" http://localhost:3000/nominals | jq > json/nominals.json
-curl -X GET -H "Accept:application/json" http://localhost:3000/pilots | jq > json/pilots.json
-curl -X GET -H "Accept:application/json" http://localhost:3000/tasks | jq > json/tasks.json
+mkdir json/comp-input
+curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/comps | jq > json/comp-input/comps.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/nominals | jq > json/comp-input/nominals.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/pilots | jq > json/comp-input/pilots.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/tasks | jq > json/comp-input/tasks.json
 
 mkdir json/gap-point
 curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/allocation | jq > json/gap-point/allocation.json
