8f915e1ee87710b672ab5bf5340ab99e0bc8ae04
Phil de Joux <philderbeast@gmail.com>
2019-01-13 08:42:16 -0500

    Download json for app-view-0.7.

--- download-json.sh
+++ download-json.sh
@@ -7,27 +7,33 @@ curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/nomina
 curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/pilots | jq > json/comp-input/pilots.json
 curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/tasks | jq > json/comp-input/tasks.json
 
+mkdir json/task-length
+curl -X GET -H "Accept:application/json" http://localhost:3000/task-length/task-lengths | jq > json/task-length/task-lengths.json
+
 mkdir json/gap-point
 curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/allocation | jq > json/gap-point/allocation.json
 curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/validity | jq > json/gap-point/validity.json
 curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/pilots-status | jq > json/gap-point/pilots-status.json
 
 mkdir json/pilot-track
-mkdir json/cross-zone
-mkdir json/task-length
 for t in {1..7}
     do
-        mkdir json/cross-zone/$t
-        curl -X GET -H "Accept:application/json" http://localhost:3000/cross-zone/$t/pilot-dnf | jq > json/cross-zone/$t/pilot-dnf.json
-        curl -X GET -H "Accept:application/json" http://localhost:3000/cross-zone/$t/pilot-nyp | jq > json/cross-zone/$t/pilot-nyp.json
+        mkdir json/comp-input/$t
+        curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/$t/pilot-abs | jq > json/comp-input/$t/pilot-abs.json
+        curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/$t/pilot-df | jq > json/comp-input/$t/pilot-df.json
+        curl -X GET -H "Accept:application/json" http://localhost:3000/comp-input/$t/pilot-dfnt | jq > json/comp-input/$t/pilot-dfnt.json
 
         mkdir json/gap-point/$t
+        curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/pilot-nyp | jq > json/gap-point/$t/pilot-nyp.json
         curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/validity-working | jq > json/gap-point/$t/validity-working.json
         curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/score | jq > json/gap-point/$t/score.json
-        curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/pilot-df | jq > json/gap-point/$t/pilot-df.json
 
         mkdir json/task-length/$t
         curl -X GET -H "Accept:application/json" http://localhost:3000/task-length/$t/spherical-edge | jq > json/task-length/$t/spherical-edge.json
+        curl -X GET -H "Accept:application/json" http://localhost:3000/task-length/$t/ellipsoid-edge | jq > json/task-length/$t/ellipsoid-edge.json
+        curl -X GET -H "Accept:application/json" http://localhost:3000/task-length/$t/projected-edge-spherical | jq > json/task-length/$t/projected-edge-spherical.json
+        curl -X GET -H "Accept:application/json" http://localhost:3000/task-length/$t/projected-edge-ellipsoid | jq > json/task-length/$t/projected-edge-ellipsoid.json
+        curl -X GET -H "Accept:application/json" http://localhost:3000/task-length/$t/projected-edge-planar | jq > json/task-length/$t/projected-edge-planar.json
 
         mkdir json/pilot-track/$t
         for p in {1..108}
